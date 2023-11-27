6989f4f17e5530dd6580cd6ad3e743fd18254100
BhupeshB7 <bhupeshkr2912@gmail.com>
2023-11-27 09:36:59 +0530

    update

--- src/pages/AdminBalance.js
+++ src/pages/AdminBalance.js
@@ -2,32 +2,42 @@
 
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Button } from 'react-bootstrap';
+import { Button, Alert } from 'react-bootstrap';
 
 function AdminBalance() {
   const [userId, setUserId] = useState('');
   const [name, setName] = useState('');
   const [balance, setBalance] = useState('');
   const [isUpdateDisabled, setIsUpdateDisabled] = useState(true);
+  const [alert, setAlert] = useState({ show: false, type: '', message: '' });
 
   const searchUser = async () => {
     try {
       const response = await axios.get(`https://mlm-production.up.railway.app/api/game/balance/${userId}`);
       setName(response.data.name);
-      setIsUpdateDisabled(false); // Enable the "Update Balance" button when the name is available
+      setIsUpdateDisabled(false);
+      setAlert({ show: false, type: '', message: '' }); // Reset alert state
     } catch (error) {
       console.error(error);
       setName('User not found');
       setIsUpdateDisabled(true);
+      setAlert({ show: true, type: 'danger', message: 'User not found' });
     }
   };
 
   const updateBalance = async () => {
     try {
+      if (!balance || isNaN(balance)) {
+        setAlert({ show: true, type: 'danger', message: 'Balance should be a valid number' });
+        return;
+      }
+
       await axios.put(`https://mlm-production.up.railway.app/api/balance/${userId}`, { balance });
+      setAlert({ show: true, type: 'success', message: 'Balance updated successfully' });
       // Optionally, you can fetch and display the updated user details
     } catch (error) {
       console.error(error);
+      setAlert({ show: true, type: 'danger', message: 'Failed to update balance' });
     }
   };
 
@@ -35,28 +45,43 @@ function AdminBalance() {
     <div className='Update_Balance mb-5'>
       <h5>User Balance Update...</h5>
       <div>
-      <label className='text-light'>
-        Enter User ID:
-      </label><br/>
+        <label className='text-light'>Enter User ID:</label><br />
         <input type="text" value={userId} onChange={(e) => setUserId(e.target.value)} />
-      <Button variant='primary' className='m-1' onClick={searchUser}>Search</Button>
-      
+        <Button variant='primary' className='m-1' onClick={searchUser}>
+          Search
+        </Button>
+
         <p>Name: {name}</p>
-        <label className='text-light'>
-          Enter Balance:
-          </label> <br/>
-          <input
-            type="text"
-            value={balance}
-            onChange={(e) => setBalance(e.target.value)}
-            disabled={isUpdateDisabled}
-            required
-          />
-       
-      </div>
-        <Button variant='success' className='m-1' onClick={updateBalance} disabled={isUpdateDisabled}>
+
+        <label className='text-light'>Enter Balance:</label><br />
+        <input
+          type="text"
+          value={balance}
+          onChange={(e) => setBalance(e.target.value)}
+          disabled={isUpdateDisabled}
+          required
+        />
+        <Button
+          variant='success'
+          className='m-1'
+          onClick={updateBalance}
+          disabled={isUpdateDisabled}
+        >
           Update Balance
         </Button>
+
+        {isUpdateDisabled && (
+          <Alert variant="info" className='AlertBalance' onClose={() => setAlert({ show: false, type: '', message: '' })} dismissible>
+            Button is disabled
+          </Alert>
+        )}
+
+        {alert.show && (
+          <Alert variant={alert.type} className='AlertBalance' onClose={() => setAlert({ show: false, type: '', message: '' })} dismissible>
+            {alert.message}
+          </Alert>
+        )}
+      </div>
     </div>
   );
 }
